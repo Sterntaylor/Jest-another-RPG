7e7bf36d184e301747babcd371b67ebcb92facc4
Taylor Stern <sterntaylor0629@gmail.com>
2020-10-01 15:24:57 -0700

    10.2.7

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -3,16 +3,46 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 
-const Player = require('../lib/Player');
-test('creates a player object', () => {
+function Player(name = '') {
+  this.name = name;
+
+  this.health = Math.floor(Math.random() * 10 + 95);
+  this.strength = Math.floor(Math.random() * 5 + 7);
+  this.agility = Math.floor(Math.random() * 5 + 7);
+
+  this.inventory = [new Potion('health'), new Potion()];
+}
+
+  Player.prototype.getStats = function() {
+    return {
+      potions: this.inventory.length,
+      health: this.health,
+      strength: this.strength,
+      agility: this.agility
+    };
+  };
+  
+  Player.prototype.getInventory = function() {
+    if (this.inventory.length) {
+      return this.inventory;
+    }
+    return false;
+  };
+
+  test("gets player's stats as an object", () => {
     const player = new Player('Dave');
   
-    expect(player.name).toBe('Dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
-
-    expect(player.inventory).toEqual(
-      expect.arrayContaining([expect.any(Object)])
-    );
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
   });
